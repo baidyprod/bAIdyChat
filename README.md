ce60e7d0e33aa47c6ab98ae37399b5a196c56374
baidyprod <dima220920041@gmail.com>
2023-05-12 02:08:38 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# AI_Chat
+# AI Chat by baidy
+
+This is an AI Chatbot by baidy
+
+## Let's create a new conversation
+![Creating a new conversation](https://res.cloudinary.com/dbtmzypoa/image/upload/v1683846326/AI%20Chat%20screenshots/kdxzk0xgxjkvvx9brgyw.png)
+
+## Let's retrieve the previous conversation context
+![Retrieving previous conversations](https://res.cloudinary.com/dbtmzypoa/image/upload/v1683846326/AI%20Chat%20screenshots/poezazpzvshkcugcy9gf.png)
---
+++ chatbot.py
@@ -0,0 +1,71 @@
+import json
+import os
+
+from langchain.chains import ConversationChain
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ChatMessageHistory
+from langchain.memory import ConversationBufferMemory
+from langchain.schema import messages_from_dict, messages_to_dict
+
+history = ChatMessageHistory()
+
+llm = ChatOpenAI(
+    temperature=0,
+    model_name="gpt-3.5-turbo",
+)
+
+chat_files = [f for f in os.listdir("chats") if f.endswith(".json")]
+print("Available chats:")
+if len(chat_files) == 0:
+    print("No saved chats found")
+for i, filename in enumerate(chat_files):
+    print(f"{i+1}. {filename}")
+
+
+new_chat_or_existing = input('Enter the full chat name or "new": ')
+if new_chat_or_existing == "new":
+    conversation = ConversationChain(llm=llm, memory=ConversationBufferMemory())
+
+    while True:
+        user_input = input('User (type "exit" to save and exit): ')
+        if user_input != "exit":
+            conversation.predict(input=user_input)
+            print(f"AI: {conversation.memory.chat_memory.messages[-1].content}")
+
+            history.add_user_message(user_input)
+            history.add_ai_message(conversation.memory.chat_memory.messages[-1].content)
+
+        else:
+            conversation_history = messages_to_dict(history.messages)
+            name_of_file = input("How we should call the chat? (conversation_name.json)? ")
+            with open(f"chats/{name_of_file}", "w") as f:
+                json.dump(conversation_history, f)
+
+            break
+
+else:
+    with open(f"chats/{new_chat_or_existing}") as f:
+        chat_history = json.load(f)
+
+    conversation = ConversationChain(llm=llm, memory=ConversationBufferMemory(chat_memory=ChatMessageHistory(
+                messages=messages_from_dict(chat_history)
+            )
+        ),
+    )
+
+    while True:
+        user_input = input('User (type "exit" to save and exit): ')
+        if user_input != "exit":
+            conversation.predict(input=user_input)
+            print(f"AI: {conversation.memory.chat_memory.messages[-1].content}")
+
+            history.add_user_message(user_input)
+            history.add_ai_message(conversation.memory.chat_memory.messages[-1].content)
+
+        else:
+            conversation_history = messages_to_dict(history.messages)
+            chat_history.append(conversation_history)
+            with open(f"chats/{new_chat_or_existing}", "w") as f:
+                json.dump(chat_history, f)
+
+            break
