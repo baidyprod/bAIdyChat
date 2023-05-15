293fc6eae617ea664bd22a4955708e76a0ca0a67
baidyprod <dima220920041@gmail.com>
2023-05-15 13:22:29 +0300

    Small style changes

--- chatbot.py
+++ chatbot.py
@@ -7,6 +7,16 @@ from langchain.memory import ChatMessageHistory
 from langchain.memory import ConversationBufferMemory
 from langchain.schema import messages_from_dict, messages_to_dict
 
+
+chat_files = [f for f in os.listdir("chats") if f.endswith(".json")]
+print("Available chats:")
+if len(chat_files) == 0:
+    print("No saved chats found")
+for i, filename in enumerate(chat_files):
+    print(f"{i+1}. {filename[:-5]}")
+
+new_chat_or_existing = input('Enter the chat name or "new": ')
+
 history = ChatMessageHistory()
 
 llm = ChatOpenAI(
@@ -14,15 +24,6 @@ llm = ChatOpenAI(
     model_name="gpt-3.5-turbo",
 )
 
-chat_files = [f for f in os.listdir("chats") if f.endswith(".json")]
-print("Available chats:")
-if len(chat_files) == 0:
-    print("No saved chats found")
-for i, filename in enumerate(chat_files):
-    print(f"{i+1}. {filename}")
-
-
-new_chat_or_existing = input('Enter the full chat name or "new": ')
 if new_chat_or_existing == "new":
     conversation = ConversationChain(llm=llm, memory=ConversationBufferMemory())
 
@@ -37,14 +38,14 @@ if new_chat_or_existing == "new":
 
         else:
             conversation_history = messages_to_dict(history.messages)
-            name_of_file = input("How we should call the chat? (conversation_name.json)? ")
-            with open(f"chats/{name_of_file}", "w") as f:
+            name_of_file = input("How we should call the chat? (conversation_name)? ")
+            with open(f"chats/{name_of_file}.json", "w") as f:
                 json.dump(conversation_history, f)
 
             break
 
 else:
-    with open(f"chats/{new_chat_or_existing}") as f:
+    with open(f"chats/{new_chat_or_existing}.json") as f:
         chat_history = json.load(f)
 
     conversation = ConversationChain(
@@ -64,7 +65,7 @@ else:
         else:
             conversation_history = messages_to_dict(history.messages)
             chat_history.append(conversation_history)
-            with open(f"chats/{new_chat_or_existing}", "w") as f:
+            with open(f"chats/{new_chat_or_existing}.json", "w") as f:
                 json.dump(chat_history, f)
 
             break
